41e9b85ba1b483e8b7c4fd142e7c0b63da922ee2
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-06-10 15:12:06 -0600

    Initial Commit

---
+++ heart proyect/heart_proyectApp.swift
@@ -0,0 +1,17 @@
+//
+//  heart_proyectApp.swift
+//  heart proyect
+//
+//  Created by Benjamin Rojo on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct heart_proyectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
